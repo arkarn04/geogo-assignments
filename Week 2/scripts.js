6d474619a92f24ff28b3ed5189420a7a24365792
arkarn04 <karnaryan253@gmail.com>
2021-12-11 21:26:09 +0530

    check

--- Week 2/scripts.js
+++ Week 2/scripts.js
@@ -1,33 +1,33 @@
+const submitBtn = document.getElementById("subBtn");
 const inputEl = document.getElementById("cityName");
 const appid = "28b782780703cad6c6e724965189ef98";
-inputEl.addEventListener("search", doSearch);
 
-function doSearch() {
-    const city = inputEl.value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}`)
-        .then((response)=>response.json())
-        .then((data)=> alert(data))
-        .catche(err => alert(err))
-}
-
-
-// $(document).ready(function() {
-//     $('#cityName').on('search', function() {
-//         const city = $('#cityName').val();
-//         const appid = "28b782780703cad6c6e724965189ef98";
-//         $.ajax({
-//             method: "GET",
-//             url: `api.openweathermap.org/data/2.5/weather?q=${city}&appid=28b782780703cad6c6e724965189ef98&units=metric`,
-//             dataType: 'JSON'
-//         })
-//         .done(function(data) {
-//             alert(data);
-//         })
-//         .fail(function(err) {
-//             alert("FAilEd");
-//         })
-//     })
+// submitBtn.addEventListener("click", function() {
+//     const city = inputEl.value;
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1800de0bf219df68408ed0e18701da7f`)
+//         .then((response)=>response.json())
+//         .then((data)=> console.log(data))
+//         .catch(err => {alert(err)});
 // })
+
+
+$(document).ready(function() {
+    $('#submitBtn').on('click', function() {
+        const city = $('#cityName').val();
+        const appid = "28b782780703cad6c6e724965189ef98";
+        $.ajax({
+            method: "GET",
+            url: `api.openweathermap.org/data/2.5/weather?q=${city}&appid=28b782780703cad6c6e724965189ef98&units=metric`,
+            dataType: 'JSON'
+        })
+        .done(function(data) {
+            console.log(data);
+        })
+        .fail(function(err) {
+            console.log("FAilEd");
+        })
+    })
+})
 
 
 
